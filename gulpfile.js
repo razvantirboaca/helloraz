c7d9c77f750ce25fb3dea47d4549f27c1f343441
Razvan Tirboaca <razvan.tirboaca@gmail.com>
2016-12-07 21:48:01 +0100

    change headline

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,80 @@
 var gulp = require('gulp'),
-  plumber = require('gulp-plumber'),
-  sass = require('gulp-sass'),
-  cssnano = require('gulp-cssnano'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  notify  = require('gulp-notify'),
-  del = require('del'),
-  bs = require('browser-sync').create(),
-  proxyUrl = 'localhost/razvan';
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    notify  = require('gulp-notify'),
+    del = require('del'),
+    bs = require('browser-sync').create(),
+    proxyUrl = 'localhost/razvan';
 
 var paths = {
-  watch : {
-    sass: 'src/css/**/**/*.scss',
-    js: 'src/js/**/**/*.js'
-  },
-  src: {
-    css: 'src/css/style.scss',
-    js: 'src/js/',
-    node: 'node_modules/'
-  },
-  dist: 'dist/'
+    watch : {
+        sass: 'src/css/**/**/*.scss',
+        js: 'src/js/**/**/*.js'
+    },
+    src: {
+        css: 'src/css/style.scss',
+        js: 'src/js/',
+        node: 'node_modules/'
+    },
+    dist: 'dist/'
 };
 
 gulp.task('browser-sync', function() {
 bs.init({
-proxy: proxyUrl,
-  scriptPath: function (path, port, options) {
-    return options.get("absolute");
-  },
-  files: ['./index.html', '!' + paths.dist + 'css/*']
-});
+    proxy: proxyUrl,
+        scriptPath: function (path, port, options) {
+            return options.get("absolute");
+        },
+        files: ['./index.html', '!' + paths.dist + 'css/*']
+    });
 });
 
-gulp.task('css', function () {
+gulp.task('sass', function () {
+var onError = function(err) {
+notify.onError({
+    title: 'Sass error',
+    message: 'Error: <%= error.message %>'
+})(err);
+this.emit('end');
+};
 return gulp.src(paths.src.css)
-  .pipe(plumber({
-    errorHandler: function (err) {
-      console.log(err);
-      this.emit('end');
-    }
-  }))
-  .pipe(sass())
-  .pipe(postcss([
-    autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    })
-  ]))
-  .pipe(cssnano())
-  .pipe(gulp.dest(paths.dist + 'css'))
-  .pipe(notify({ message: 'CSS task complete' }))
-  .pipe(bs.stream())
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass())
+    .pipe(postcss([
+        autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        })
+    ]))
+    .pipe(cssnano())
+    .pipe(gulp.dest(paths.dist + 'css'))
+    .pipe(notify({ message: 'Sass task complete' }))
+    .pipe(bs.stream())
 });
 
 gulp.task('js', function () {
 return gulp.src([
-    paths.src.node + 'photoswipe/dist/photoswipe.min.js',
-    paths.src.node + 'photoswipe/dist/photoswipe-ui-default.min.js',
-    //paths.src.node + 'sticky-state/dist/sticky-state.min.js',
-    paths.src.js + 'app.js'
-  ])
-  .pipe(concat('app.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(paths.dist + 'js'))
-  .pipe(notify({ message: 'JS task complete' }))
-  .pipe(bs.stream())
+        paths.src.node + 'photoswipe/dist/photoswipe.min.js',
+        paths.src.node + 'photoswipe/dist/photoswipe-ui-default.min.js',
+        //paths.src.node + 'sticky-state/dist/sticky-state.min.js',
+        paths.src.js + 'app.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist + 'js'))
+    .pipe(notify({ message: 'JS task complete' }))
+    .pipe(bs.stream())
 });
 
 gulp.task('default', ['browser-sync'], function(){
 // Listen to change events on scss and compile sass
-gulp.watch(paths.watch.sass, ['css']);
+gulp.watch(paths.watch.sass, ['sass']);
 // Listen to change events on js and concat js
 gulp.watch(paths.watch.js, ['js']);
 });
