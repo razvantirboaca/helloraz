a2f1f237fc628abe7da04b0493138d0111e68c87
RazvanTirboaca <razvan.tirboaca@gmail.com>
2021-04-07 10:17:11 +0200

    feat: creates the main js

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    (function () {
+      // Load ICONS
+      const fileName = 'icons.svg';
+      const elementId = 'js-svg-' + fileName.replace('.svg', '');
+      const ajax = new XMLHttpRequest();
+      ajax.open('GET', `./assets/svg/${fileName}`, true);
+      ajax.send();
+      ajax.onload = function () {
+        if (document.getElementById(elementId)) return;
+        const div = document.createElement('div');
+        div.id = elementId;
+        div.style.display = 'none';
+        div.innerHTML = ajax.responseText;
+        document.body.appendChild(div);
+      };
+    })();
+})();
