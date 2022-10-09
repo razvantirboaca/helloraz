4549a5d9e39cf49ab5fe1fa8a6719eacb1a676cb
Raz <razvan.tirboaca@gmail.com>
2022-10-09 18:03:21 +0200

    Feat: Adds and styles copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,20 @@ module.exports = {
     './src/**/*.{njk, md, html}'
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Inter', defaultTheme.fontFamily.sans],
-        'handwriting': 'Caveat, cursive',
-      }
-    }
-  }
+    fontFamily: {
+      'sans': ['Figtree', defaultTheme.fontFamily.sans],
+      'handwriting': 'Caveat, cursive',
+    },
+    colors: {
+      'transparent': 'transparent',
+      'primary': 'rgb(255 255 255 / 96%)',
+      'primary-weak': 'rgb(255 255 255 / 36%)',
+      'primary-medium': 'rgb(255 255 255 / 64%)',
+      'primary-strong': '#090c07',
+      'secondary': '#b1d255',
+    },
+    backgroundImage: {
+      'primary': 'linear-gradient(315deg, #00CDAC 0%, #02AAB0 74%)',
+    },
+  },
 }
