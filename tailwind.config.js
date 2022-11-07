47ac2541d9f207f31347fda67be10b4034a282b8
Raz <razvan.tirboaca@gmail.com>
2022-11-07 16:31:10 +0100

    Style: Adds shadow variables and tweaks neutrals

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,11 @@ module.exports = {
       'primary': 'var(--color-primary)',
       'secondary': 'var(--color-secondary)',
     },
+    boxShadow: {
+      'sm': '0.2px 0.3px 0.4px hsl(var(--shadow-color) / 0.24), 0.2px 0.5px 0.6px -1.2px hsl(var(--shadow-color) / 0.24), 0.5px 1.2px 1.5px -2.5px hsl(var(--shadow-color) / 0.24)',
+      'md': '0.2px 0.3px 0.4px hsl(var(--shadow-color) / 0.25), 0.5px 1px 1.3px -0.8px hsl(var(--shadow-color) / 0.25), 1.1px 2.4px 3px -1.7px hsl(var(--shadow-color) / 0.25), 2.7px 5.8px 7.2px -2.5px hsl(var(--shadow-color) / 0.25)',
+      'lg': '0.2px 0.3px 0.4px hsl(var(--shadow-color) / 0.21), 0.6px 1.3px 1.6px -0.3px hsl(var(--shadow-color) / 0.21), 1.1px 2.3px 2.9px -0.6px hsl(var(--shadow-color) / 0.21), 1.7px 3.6px 4.5px -0.9px hsl(var(--shadow-color) / 0.21), 2.5px 5.4px 6.7px -1.2px hsl(var(--shadow-color) / 0.21), 3.8px 8.1px 10.1px -1.6px hsl(var(--shadow-color) / 0.21), 5.5px 11.8px 14.6px -1.9px hsl(var(--shadow-color) / 0.21), 7.9px 16.9px 21px -2.2px hsl(var(--shadow-color) / 0.21), 11px 23.7px 29.4px -2.5px hsl(var(--shadow-color) / 0.21)',
+    },
     extend: {
       fontSize: {
         'lg': ['1.05rem', '1.65'],
@@ -38,20 +43,22 @@ module.exports = {
       addBase({
         ':root': {
           '--color-neutral': 'rgb(252 252 252)',
-          '--color-neutral-weakest': 'rgb(26 30 32 / 16%)',
-          '--color-neutral-weak': 'rgb(26 30 32 / 32%)',
+          '--color-neutral-weakest': 'rgb(26 30 32 / 4%)',
+          '--color-neutral-weak': 'rgb(26 30 32 / 16%)',
           '--color-neutral-medium': 'rgb(26 30 32 / 82%)',
           '--color-neutral-strong': 'rgb(26 30 32)',
           '--color-primary': 'rgb(52 168 71)',
           '--color-secondary': 'rgb(251 126 168)',
+          '--shadow-color': '0deg 0% 72%',
         },
         '@media (prefers-color-scheme: dark)': {
           ':root': {
             '--color-neutral': 'rgb(1 9 32)',
-            '--color-neutral-weakest': 'rgb(255 255 255 / 16%)',
-            '--color-neutral-weak': 'rgb(255 255 255 / 24%)',
+            '--color-neutral-weakest': 'rgb(255 255 255 / 4%)',
+            '--color-neutral-weak': 'rgb(255 255 255 / 16%)',
             '--color-neutral-medium': 'rgb(255 255 255 / 80%)',
             '--color-neutral-strong': 'rgb(255 255 255 / 94%)',
+            '--shadow-color': '225deg 94% 6%',
           }
         }
       })
