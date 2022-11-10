97a3add19afcfd31bd8a4a583a7fe1a8eaaeb554
Raz <razvan.tirboaca@gmail.com>
2022-11-10 14:02:41 +0100

    Feat: Adds work

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,6 @@ module.exports = {
     './src/**/*.{njk, md, html}'
   ],
   theme: {
-    fontFamily: {
-      'sans': ['Figtree', defaultTheme.fontFamily.sans],
-      'handwriting': 'Caveat, cursive',
-    },
     colors: {
       'transparent': 'transparent',
       'neutral': 'var(--color-neutral)',
@@ -33,13 +29,18 @@ module.exports = {
       'lg': '0.2px 0.3px 0.4px hsl(var(--shadow-color) / 0.21), 0.6px 1.3px 1.6px -0.3px hsl(var(--shadow-color) / 0.21), 1.1px 2.3px 2.9px -0.6px hsl(var(--shadow-color) / 0.21), 1.7px 3.6px 4.5px -0.9px hsl(var(--shadow-color) / 0.21), 2.5px 5.4px 6.7px -1.2px hsl(var(--shadow-color) / 0.21), 3.8px 8.1px 10.1px -1.6px hsl(var(--shadow-color) / 0.21), 5.5px 11.8px 14.6px -1.9px hsl(var(--shadow-color) / 0.21), 7.9px 16.9px 21px -2.2px hsl(var(--shadow-color) / 0.21), 11px 23.7px 29.4px -2.5px hsl(var(--shadow-color) / 0.21)',
     },
     extend: {
+      fontFamily: {
+        'sans': ['Figtree', defaultTheme.fontFamily.sans],
+        'cursive': 'Caveat, cursive',
+      },
       fontSize: {
-        'lg': ['1.135rem', '1.6'],
-        'xl': ['1.25rem', '1.6'],
+        'lg': ['1.135rem', '1.5'],
+        'xl': ['1.25rem', '1.5'],
         '2xl': ['1.515rem', '1.35'],
-        '3xl': ['1.75rem', '1.2'],
+        '3xl': ['1.75rem', '1.22'],
         '4xl': ['2.25rem', '1.2'],
         '5xl': ['2.75rem', '1.15'],
+        '6xl': ['2.9rem', '1.05'],
       },
     },
   },
@@ -49,22 +50,12 @@ module.exports = {
         ':root': {
           '--color-neutral': 'hsl(0deg 0% 100%)',
           '--color-neutral-weakest': 'hsl(210deg 12% 98%)',
-          '--color-neutral-weak': 'hsl(210deg 4% 80%)',
-          '--color-neutral-medium': 'hsl(210deg 4% 48%)',
+          '--color-neutral-weak': 'hsl(210deg 8% 92%)',
+          '--color-neutral-medium': 'hsl(210deg 8% 46%)',
           '--color-neutral-strong': 'hsl(210deg 4% 4%)',
           '--color-primary': 'rgb(52 168 71)',
           '--color-secondary': 'rgb(251 126 168)',
-          '--shadow-color': '210deg 4% 72%',
-        },
-        '@media (prefers-color-scheme: dark)': {
-          ':root': {
-            '--color-neutral': 'rgb(1 9 32)',
-            '--color-neutral-weakest': 'rgb(255 255 255 / 6%)',
-            '--color-neutral-weak': 'rgb(255 255 255 / 16%)',
-            '--color-neutral-medium': 'rgb(255 255 255 / 72%)',
-            '--color-neutral-strong': 'rgb(255 255 255 / 94%)',
-            '--shadow-color': '225deg 94% 6%',
-          }
+          '--shadow-color': '210deg 20% 72%',
         }
       })
     })
